38e3b2b8c7704e82c112fb3f76631af465e13e16
AlphaMugisha <tigerkev07@gmail.com>
2026-05-09 18:33:45 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <h1 className="hero">Karibu Simba Supermarket!</h1>
+
+      <p className="hero">
+        Fresh groceries, delivered across Kigali.
+      </p>
+
+      <img
+        className="hero"
+        src="https://placehold.co/600x300"
+        alt="Simba Supermarket"
+      />
+    </>
+  );
+}
+
+export default App;
