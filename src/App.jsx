02c1c399753eb2572e936b13f497ce0399401a1f
AlphaMugisha <tigerkev07@gmail.com>
2026-05-10 11:54:07 +0200

    adding

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,15 @@
 import "./App.css";
 
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
-      <nav className="navbar">
-        <h2 className="logo">Simba Supermarket</h2>
-
-        <div className="nav-links">
-          <a href="#">Home</a>
-          <a href="#">Products</a>
-          <a href="#">Offers</a>
-          <a href="#">Contact</a>
-        </div>
-      </nav>
-
-      <section className="hero">
-        <div className="hero-content">
-          <h1>Karibu Simba Supermarket!</h1>
-
-          <p>
-            Fresh groceries, snacks, drinks, and household essentials
-            delivered across Kigali at lightning speed.
-          </p>
-
-          <button>Shop Now</button>
-        </div>
-
-        <div className="hero-image-container">
-          <img
-            src="https://images.unsplash.com/photo-1542838132-92c53300491e"
-            alt="Supermarket"
-          />
-        </div>
-      </section>
+      <Header />
+      <Hero />
+      <Footer />
     </>
   );
 }
