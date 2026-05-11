efe846fa980eba87b0f62b1c2e265a05b05a8e89
AlphaMugisha <tigerkev07@gmail.com>
2026-05-11 11:12:52 +0200

    added another image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
       <div className="hero-visual">
         <div className="floating-image-container glass-panel">
           <img 
-            src="https://images.unsplash.com/photo-1542838132-92c53300491e?auto=format&fit=crop&q=80&w=1000" 
+            src="https://plus.unsplash.com/premium_photo-1663040589382-88caf6b2bc60?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
             alt="Fresh Produce" 
             className="spatial-img"
           />
