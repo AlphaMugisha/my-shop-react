783a76b7b2694ccd1a603856acee5c23fe01dfbe
AlphaMugisha <tigerkev07@gmail.com>
2026-05-11 14:40:00 +0200

    adding the thing

---
+++ src/app2.jsx
@@ -0,0 +1,9 @@
+import TestFunction from "./components/react";
+export default function App2() {
+    return (
+        <div>
+            <h1>Testing API Fetch</h1>
+            <TestFunction />
+        </div>
+    );
+}
